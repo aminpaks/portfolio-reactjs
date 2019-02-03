7ef676adcc84489b518a17243fdc00259cf2c1e3
Amin Pakseresht <amin.pakseresht@hotmail.com>
2019-02-03 18:07:38 -0500

    Initial work

---
+++ src/App.styled.ts
@@ -0,0 +1,40 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+
+  a {
+    color: #61dafb;
+  }
+`;
+
+export const StyledContainer = styled.div`
+  text-align: center;
+`;
+
+export const StyledLogo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 40vmin;
+`;
+
+export const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { GlobalStyles, StyledContainer, StyledHeader, StyledLogo } from './App.styled';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <StyledContainer>
+        <GlobalStyles />
+        <StyledHeader>
+          <StyledLogo src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
-        </header>
-      </div>
+        </StyledHeader>
+      </StyledContainer>
     );
   }
 }
