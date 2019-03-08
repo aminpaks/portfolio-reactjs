f480a19a810bd41f8d84efb958bd9008b60e8ce7
Amin Paks <amin.pakseresht@hotmail.com>
2019-03-07 23:28:59 -0500

    Add some minor adustments in Styleguide page

--- src/Styleguide/Page.tsx
+++ src/Styleguide/Page.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { RouteChildrenProps, Redirect } from 'react-router';
 
-import { Layout, LayoutSection } from './+Layout';
+import { Layout } from './+Layout';
 import { ButtonsRoute } from './Buttons';
 import { CarouselsRoute } from './Carousels';
 import { GridSystemRoute } from './GridSystem';
@@ -10,24 +10,27 @@ import { SectionsRoute } from './Sections';
 import { TabsRoute } from './Tabs';
 import { TypographiesRoute } from './Typographies';
 
+const reTrailingSlash = /\/$/;
+const routes = [
+  { title: '<< Exit StyleGuide >>', url: '../' },
+  { title: 'Buttons', url: './buttons' },
+  { title: 'Carousels', url: './carousels' },
+  { title: 'Grid System', url: './grid-system' },
+  { title: 'Panels', url: './panels' },
+  { title: 'Sections', url: './sections' },
+  { title: 'Tabs', url: './tabs' },
+  { title: 'Typographies', url: './typographies' },
+];
+
 export const StyleGuide: FC<RouteChildrenProps> = props => {
   const { location } = props;
-  const isStyleguideRoot = /\/styleguide\/?$/i.test(location.pathname);
+  const shouldRedirect =
+    /\/styleguide\/?$/i.test(location.pathname) ||
+    routes.some(route => location.pathname.replace(reTrailingSlash, '').endsWith(route.url)) === false;
 
   return (
-    <Layout
-      navItems={[
-        { title: '<< Exit StyleGuide >>', url: '../' },
-        { title: 'Buttons', url: './buttons' },
-        { title: 'Carousels', url: './carousels' },
-        { title: 'Grid System', url: './grid-system' },
-        { title: 'Panels', url: './panels' },
-        { title: 'Sections', url: './sections' },
-        { title: 'Tabs', url: './tabs' },
-        { title: 'Typographies', url: './typographies' },
-      ]}
-    >
-      {isStyleguideRoot && <Redirect to="/styleguide/buttons" />}
+    <Layout navItems={routes}>
+      {shouldRedirect && <Redirect to="/styleguide/buttons" />}
       <ButtonsRoute {...props} />
       <CarouselsRoute {...props} />
       <GridSystemRoute {...props} />
