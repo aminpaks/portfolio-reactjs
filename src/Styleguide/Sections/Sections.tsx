ac4062c2946a286d2a4989831b4f86c9efdf1585
Amin Paks <amin.pakseresht@hotmail.com>
2019-03-14 03:11:46 -0400

    Update Section UI-Kits component

--- src/Styleguide/Sections/Sections.tsx
+++ src/Styleguide/Sections/Sections.tsx
@@ -22,8 +22,8 @@ export const Sections: FC = () => (
     <Section backgroundTheme="light-gray">
       <Typography variant="headline">Default Section with gray background theme</Typography>
       <Typography variant="body">
-        it's important to mix it with different background theme. This way is easier to distinguish
-        each section from the next one.
+        it's important to mix it with different background theme. This way is easier to distinguish each section from
+        the next one.
         <code>
           <pre>{`<Section backgroundTheme="light-gray">
   <Typography variant="body">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
@@ -33,14 +33,14 @@ export const Sections: FC = () => (
     </Section>
 
     <Section>
-      <Typography variant="headline">Dummy section</Typography>
+      <Typography variant="subheading">Dummy White Section</Typography>
     </Section>
 
     <Section backgroundTheme="accent">
       <Typography variant="headline">Default Section with accent background theme</Typography>
       <Typography variant="body">
-        it's important to mix it with different background theme. This way is easier to distinguish
-        each section from the next one.
+        it's important to mix it with different background theme. This way is easier to distinguish each section from
+        the next one.
         <code>
           <pre>{`<Section backgroundTheme="light-gray">
   <Typography variant="body">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
@@ -53,8 +53,7 @@ export const Sections: FC = () => (
       <div style={{ maxWidth: '600px', padding: '0 2em', margin: '0 auto' }}>
         <Typography variant="headline">Fluid Section</Typography>
         <Typography variant="body">
-          Fluid variant doesn't limit the width of the content so you have more flexibility over
-          styling the content:
+          Fluid variant doesn't limit the width of the content so you have more flexibility over styling the content:
           <code>
             <pre>{`<Section variant="fluid">
   <div style={{ maxWidth: '600px', padding: '0 2em', margin: '0 auto' }}>
@@ -64,6 +63,50 @@ export const Sections: FC = () => (
           </code>
         </Typography>
       </div>
+    </Section>
+
+    <Section backgroundTheme={['#042E5E', '#05548C']}>
+      <Typography variant="headline">Section with Custom Background Color</Typography>
+      <Typography variant="body">
+        Code sample:
+        <code>
+          <pre>{`<Section backgroundTheme={['#042E5E', '#05548C']}>
+  <Typography variant="body">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+</Section>`}</pre>
+        </code>
+      </Typography>
+    </Section>
+
+    <Section>
+      <Typography variant="subheading">Dummy White Section</Typography>
+    </Section>
+
+    <Section variant="right-round" backgroundTheme="accent">
+      <Typography variant="headline">Section with Right Space</Typography>
+      <Typography variant="body">
+        Code sample:
+        <code>
+          <pre>{`<Section variant="right-space" backgroundTheme="accent">
+  <Typography variant="body">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+</Section>`}</pre>
+        </code>
+      </Typography>
+    </Section>
+
+    <Section>
+      <Typography variant="subheading">Dummy White Section</Typography>
+    </Section>
+
+    <Section variant="left-round" backgroundTheme="accent">
+      <Typography variant="headline">Section with Left Space</Typography>
+      <Typography variant="body">
+        Code sample:
+        <code>
+          <pre>{`<Section variant="left-space" backgroundTheme="accent">
+  <Typography variant="body">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</Typography>
+</Section>`}</pre>
+        </code>
+      </Typography>
     </Section>
   </>
 );
--- src/UI-Kits/Section/Section.styled.ts
+++ src/UI-Kits/Section/Section.styled.ts
@@ -1,97 +1,40 @@
-import styled, { WithThemeProps, lighten } from 'src/Theme';
-import { mediaQuery, breakpoints } from 'src/Device';
+import styled from 'src/Theme';
+import { mediaQuery } from 'src/Device';
 
 import { SectionProps } from './Section';
-import { StyledPanel } from '../Panel/Panel.styled';
-import { StyledTypography } from '../Typography/Typography.styled';
-
-const setWidthValue = ({ variant }: SectionProps) => {
-  switch (variant) {
-    case 'fluid':
-      return null;
-    case 'default':
-    default:
-      return `
-${mediaQuery.tabletLandscapeMin} { width: calc(${breakpoints.tabletLandscape}px - 4.8rem); }
-${mediaQuery.desktopMin} { width: calc(${breakpoints.desktop}px - 4.8rem); }
-      `;
-  }
-};
-
-const getMarginValue = ({ variant }: SectionProps) => {
-  switch (variant) {
-    case 'fluid':
-      return null;
-    case 'default':
-    default:
-      return '0 auto';
-  }
-};
-
-const setPaddingValue = ({ variant }: SectionProps) => {
-  switch (variant) {
-    case 'fluid':
-      return null;
-    case 'default':
-    default:
-      return `
-padding-left: 1.2rem;
-padding-right: 1.2rem;
-
-${mediaQuery.tabletPortraitMin} {
-  padding-left: 2.4rem;
-  padding-right: 2.4rem;
-}
-    `;
-  }
-};
-
-const setBackgroundColor = ({
-  backgroundTheme,
-  theme: { colorSet },
-}: WithThemeProps<SectionProps>) => {
-  switch (backgroundTheme) {
-    case 'light-gray':
-      return `background-color: ${lighten(colorSet.gray, 1.14)}`;
-
-    case 'accent':
-      return `
-background-image: linear-gradient(to top, ${colorSet.secondary}, ${lighten(
-        colorSet.secondary,
-        0.1,
-      )});
-color: ${colorSet.textInverted};
-${StyledPanel} {
-  color: ${colorSet.text};
-}
-${StyledTypography}[data-variant="body"] {
-  font-weight: 700;
-}
-${StyledPanel} ${StyledTypography}[data-variant="body"] {
-  font-weight: 400;
-}
-      `;
-
-    case 'default':
-    default:
-      return null;
-  }
-};
+import { setBackground, setColor, setPaddingValue, setWidthValue, getMarginValue, setRootBox } from './Utils';
 
 export const StyledSection = styled.section<SectionProps>`
-  padding-top: 2em;
-  padding-bottom: 3em;
-  ${mediaQuery.desktopMin} {
-    padding-top: 6em;
-    padding-bottom: 7.8em;
-  }
-
-  ${setBackgroundColor};
+  ${setRootBox};
 
   > div {
-    ${setPaddingValue};
-    ${setWidthValue};
-    margin: ${getMarginValue};
     position: relative;
+    padding-top: 2em;
+    padding-bottom: 3em;
+    ${mediaQuery.desktopMin} {
+      padding-top: 6em;
+      padding-bottom: 7.8em;
+    }
+
+    &::before {
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      display: block;
+      position: absolute;
+      ${setBackground};
+    }
+
+    > div {
+      ${setColor};
+
+      > div {
+        ${setPaddingValue};
+        ${setWidthValue};
+        margin: ${getMarginValue};
+        position: relative;
+      }
+    }
   }
 `;
