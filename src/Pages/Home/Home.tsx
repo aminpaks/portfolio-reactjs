fc1f53cedddb855f797a33fe425fe02bde456ba3
Amin Paks <amin.pakseresht@hotmail.com>
2019-03-24 17:38:37 -0400

    Remove unnecessary homepage sections

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -6,17 +6,15 @@ import { Section } from 'src/UI-Kits';
 import { HomeAboutSection } from './About';
 import { HomeExpertiseSection } from './Expertise';
 import { HomeMainSection } from './Main';
-import { PriceListSection } from './PriceList';
 
 export const HomePage = () => (
   <>
     <HeadTitle title="Home" />
+
     <MainLayout>
       <HomeMainSection />
       <HomeExpertiseSection />
       <HomeAboutSection />
-      <Section backgroundTheme="light-gray">Placeholder</Section>
-      <PriceListSection />
       <Section>Placeholder</Section>
     </MainLayout>
   </>
